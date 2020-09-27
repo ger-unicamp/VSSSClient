2a142408a750de6243d37b874c940ab596044e94
Your Name <you@example.com>
2020-09-27 18:38:22 -0300

    implemented hyperbolical spiral field for ball APF

--- src/strategy/APF.cpp
+++ src/strategy/APF.cpp
@@ -19,7 +19,7 @@ ctrl::vec2 apf::move_robot(fira_message::Robot &robot, ctrl::vec2 apf_vector)
 
 ctrl::vec2 apf::uniform_goal_field()
 {
-    double apf_intensity = 5;
+    double apf_intensity = 4;
 
     ctrl::vec2 apf_vector = ctrl::vec2(apf_intensity, 0.0); // potential field vector at robot position
     
@@ -30,10 +30,11 @@ ctrl::vec2 apf::uniform_walls_field(fira_message::Robot &robot)
 {
     double y = robot.y(); // force proportional to y and
     double x = -robot.x(); // inversely proportional to x coord of robot
+    int y_sgn = ctrl::sgn(y);
     double ky = 5; // force strength in y axis
-    double kx = 2.5; // force strength in x axis
+    double kx = 5; // force strength in x axis
 
-    ctrl::vec2 apf_vector = ctrl::vec2(kx*x, ky*y);
+    ctrl::vec2 apf_vector = ctrl::vec2(0, ky*y);
     
     return apf_vector;
 }
@@ -44,7 +45,7 @@ ctrl::vec2 apf::robots_field(fira_message::Robot &robot1, fira_message::Robot &r
     ctrl::vec2 apf_vector = ctrl::vec2(0.0); // potential field vector at robot position
     double dist = sqrt(pow((robot2.x() - robot1.x()), 2) + pow((robot2.y() - robot1.y()), 2)); // distance to robot2
     double angle = atan2(-(robot2.y() - robot1.y()), (robot2.y() - robot1.x())); // angle to robot2
-    double k = 0.1; // robot2 field strength scale
+    double k = 0.20; // robot2 field strength scale
 
     apf_vector.x = -k*cos(angle)/pow(dist, 2);
     apf_vector.y = -k*sin(angle)/pow(dist, 2);
@@ -56,15 +57,31 @@ ctrl::vec2 apf::robots_field(fira_message::Robot &robot1, fira_message::Robot &r
 
 ctrl::vec2 apf::ball_field(fira_message::Robot &robot, fira_message::Ball &ball)
 {
+    // hyperbolic spiral field
     ctrl::vec2 apf_vector = ctrl::vec2(0.0); // potential field vector at robot position
-    double dist = sqrt(pow((ball.x() - robot.x()), 2) + pow((ball.x() - robot.y()), 2)); // distance to ball
     double angle = atan2(-(ball.y() - robot.y()), (ball.x() - robot.x())); // angle to ball
-     
-    double k = 15.0; // ball field strength scale
-
-    apf_vector.x = k*cos(angle);
-    apf_vector.y = k*sin(angle);
+    double dist = 3*sqrt(pow((ball.x() - robot.x()), 2) + pow((ball.y() - robot.y()), 2));
+    double r = 2.0; // field center radius
+    double s = 10.0; // field smoothing scale
+    double k = 5.0; // field strenght scale
+    double phi;
     
+    // if (robot.x() < ball.x() + 0.2)
+    // {
+    //     apf_vector.x = dist*k*cos(angle);
+    //     apf_vector.y = dist*k*sin(angle);
+    // }
+        
+    // else
+    {
+        if (dist > r)
+            phi = angle + (PI/2)*(2 - (r + s)/(dist + s));
+        else if (dist >= 0 && dist <= r)
+            phi = angle + (PI/2)*sqrt(dist/r);
+        apf_vector.x = k*cos(phi);
+        apf_vector.y = k*sin(phi);
+    }
+
     // std::cout << apf_vector.x << apf_vector.y << std::endl;
     return apf_vector;
-} 
+}
--- src/strategy/APF.h
+++ src/strategy/APF.h
@@ -8,8 +8,9 @@ namespace apf
     ctrl::vec2 move_robot(fira_message::Robot &robot, ctrl::vec2 apf_vector);
     ctrl::vec2 uniform_goal_field();
     ctrl::vec2 robots_field(fira_message::Robot &robot1, fira_message::Robot &robot2);
-    ctrl::vec2 ball_field(fira_message::Robot &robot, fira_message::Ball &ball);
     ctrl::vec2 uniform_walls_field(fira_message::Robot &robot); 
+    ctrl::vec2 ball_field(fira_message::Robot &robot, fira_message::Ball &ball);
+
 };
 
 
