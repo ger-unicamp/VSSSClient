341e78f602cb61d86bc4275698761d1fefcb4165
rodrigocaus <digocaus@hotmail.com>
2020-10-25 15:26:33 -0300

    feature(controller): first implementations of future position of a Ball and a Robot.

--- src/strategy/controller.cpp
+++ src/strategy/controller.cpp
@@ -24,3 +24,57 @@ ctrl::vec2 ctrl::move_robot(fira_message::Robot &robot, ctrl::vec2 vector, doubl
 
     return motors_speed;
 }
+
+/**
+ * @brief Returns b's future position based on its speed
+ * 
+ * @param b 
+ * @param dt Time interval in seconds
+ * @return ctrl::vec2 
+ */
+ctrl::vec2 ctrl::future_position(fira_message::Ball &b, double dt)
+{
+    ctrl::vec2 path = dt * ctrl::vec2(b.vx(), b.vy());
+    return ctrl::vec2(b) + path;
+}
+
+/**
+ * @brief Returns b's future position based on its relative speed to r
+ * 
+ * @param b 
+ * @param r 
+ * @param dt Time interval in seconds
+ * @return ctrl::vec2 
+ */
+ctrl::vec2 ctrl::future_position(fira_message::Ball &b, fira_message::Robot &r, double dt)
+{
+    ctrl::vec2 path = dt * ctrl::vec2(b.vx() - r.vx(), b.vy() - r.vy());
+    return ctrl::vec2(b) + path;
+}
+
+/**
+ * @brief Returns r's future position based on its speed
+ * 
+ * @param r 
+ * @param dt Time interval in seconds
+ * @return ctrl::vec2 
+ */
+ctrl::vec2 ctrl::future_position(fira_message::Robot &r, double dt)
+{
+    ctrl::vec2 path = dt * ctrl::vec2(r.vx(), r.vy());
+    return ctrl::vec2(r) + path;
+}
+
+/**
+ * @brief Returns r1's future position based on its relative speed to r2
+ * 
+ * @param r1 
+ * @param r2 
+ * @param dt Time interval in seconds
+ * @return ctrl::vec2 
+ */
+ctrl::vec2 ctrl::future_position(fira_message::Robot &r1, fira_message::Robot &r2, double dt)
+{
+    ctrl::vec2 path = dt * ctrl::vec2(r1.vx() - r2.vx(), r1.vy() - r2.vy());
+    return ctrl::vec2(r1) + path;
+}
