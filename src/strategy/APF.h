52f8dc9e764cdfff9f11adab2956e84d67aa353b
rodrigocaus <digocaus@hotmail.com>
2020-10-24 22:28:00 -0300

    fix(APF): Fixed a signal in spiral field. Now the curve seems better.
    Remove the move robot fuction to controll (better scope i think)
    
    Refactor robots field to calculate less sines and cosines.

--- src/strategy/APF.h
+++ src/strategy/APF.h
@@ -2,10 +2,8 @@
 #define APF
 #include "strategy/controller.h"
 
-
 namespace apf
 {
-    ctrl::vec2 move_robot(fira_message::Robot &robot, ctrl::vec2 apf_vector, double k, double v);
     ctrl::vec2 uniform_goal_field();
     ctrl::vec2 robots_field(fira_message::Robot &robot1, fira_message::Robot &robot2);
     ctrl::vec2 uniform_walls_field(fira_message::Robot &robot);
