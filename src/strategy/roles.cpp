59813faafd9faa252ec83bc3c26cd383435790aa
h217638 <h217638@dac.unicamp.br>
2020-11-09 16:46:01 -0300

    Adding sectors for the attacker and defender

--- src/strategy/roles.cpp
+++ src/strategy/roles.cpp
@@ -14,67 +14,73 @@ ctrl::vec2 rol::goalkeeper(fira_message::Robot &robot, fira_message::Ball &ball)
     else
     {
         apf_vec = gpk::follow(robot, ball);
-        apf_vec = ctrl::move_robot(robot,apf_vec,0.300637,40*0.661463+20);
+        apf_vec = ctrl::move_robot(robot,apf_vec,K_TURNING, K_VEL);
     }
 
     return 6*apf_vec;
 }
 
-ctrl::vec2 rol::attacker(fira_message::Robot &robot, fira_message::Ball &ball)
+ctrl::vec2 rol::attacker(unsigned int moving_robot_id,fira_message::Ball &ball, std::vector<fira_message::Robot> my_robots, std::vector<fira_message::Robot> enemy_robots)
 {
     ctrl::vec2 apf_vec;
-    //G0:0.0922878 G1:0.00563102 G2:0.438424 G3: 40 * 0.617454 + 10)
-    apf_vec = apf::ball_field(robot, ball, 0.0755485, 0.0691405);
-    apf_vec = ctrl::move_robot(robot, apf_vec, 0.443467, 40.0 * 0.664899 + 10.0);
+    double spiral_phi = apf::move_to_goal(my_robots[moving_robot_id], ball, RADIUS, K_SPIRAL);
+    pair<double,double> tmp = apf::repulsion_field(moving_robot_id, my_robots, enemy_robots, DT);
+    double phi = apf::composite_field(tmp.first, spiral_phi, SIGMA, D_MIN, tmp.second);
+    sincos(phi,&apf_vec.y,&apf_vec.x);
+    apf_vec = ctrl::move_robot(my_robots[moving_robot_id], apf_vec, K_TURNING, K_VEL);
 
     return apf_vec;
 }
 
-ctrl::vec2 rol::defender(fira_message::Robot &robot, fira_message::Ball &ball, int i)
+ctrl::vec2 rol::defender(unsigned int moving_robot_id,fira_message::Ball &ball, std::vector<fira_message::Robot> my_robots, std::vector<fira_message::Robot> enemy_robots)
 {
-    ctrl::vec2 pos_robot = ctrl::vec2(robot);
     ctrl::vec2 pos_ball = ctrl::vec2(ball);
-    double auxx,auxy;
 
-    if (i == 0)
+    if (pos_ball.x < -0.5)
     {
-        if (pos_ball.x > 0.5)
-        {
-            auxx = -(pos_robot.x - 0.5 );
-            auxy = -(pos_robot.y - 0.3);
-        }
-        else if (pos_ball.x < -0.5)
-        {
-            auxx = -(pos_robot.x - -0.5 );
-            auxy = -(pos_robot.y - 0.3);
-        }
-        else
-        {
-            auxx = -(pos_robot.x - pos_ball.x );
-            auxy = -(pos_robot.y - 0.3);
-        } 
+        pos_ball.x = -0.5;
+    }
+    else if (pos_ball.x > 0.5)
+    {
+        pos_ball.x = 0.5;
+    }
+
+    if (moving_robot_id == 1)
+    {
+        pos_ball.y = 0.3;
     }
     else
     {
-        if (pos_ball.x > 0.5)
-        {
-            auxx = -(pos_robot.x - 0.5 );
-            auxy = -(pos_robot.y - -0.3);
-        }
-        else if (pos_ball.x < -0.5)
-        {
-            auxx = -(pos_robot.x - -0.5);
-            auxy = -(pos_robot.y - -0.3);
-        }
-        else
-        {
-            auxx = -(pos_robot.x - pos_ball.x );
-            auxy = -(pos_robot.y - -0.3);
-        } 
+        pos_ball.y = -0.3;
     }
-    ctrl::vec2 apf_vec = ctrl::vec2(auxx,auxy);
-    apf_vec = ctrl::move_robot(robot, apf_vec, 0.392803, 40.0 * 0.822646 + 10.0);
-    
-    return 5*apf_vec;
+
+    ctrl::vec2 apf_vec;
+    double spiral_phi = apf::move_to_goal(my_robots[moving_robot_id], pos_ball, RADIUS, K_SPIRAL);
+    pair<double,double> tmp = apf::repulsion_field(moving_robot_id, my_robots, enemy_robots, DT);
+    double phi = apf::composite_field(tmp.first, spiral_phi, SIGMA, D_MIN, tmp.second);
+    sincos(phi,&apf_vec.y,&apf_vec.x);
+    apf_vec = ctrl::move_robot(my_robots[moving_robot_id], apf_vec, K_TURNING, K_VEL);
+
+    return apf_vec;
 }
 
+vector<ctrl::vec2> rol::select_role(fira_message::Ball &ball,vector<fira_message::Robot> &my_robots, vector<fira_message::Robot> &enemy_robots)
+{
+    vector<ctrl::vec2> roles(3);
+    ctrl::vec2 pos_ball = ctrl::vec2(ball);
+
+    roles[0] = goalkeeper(my_robots[0], ball);
+
+    if (pos_ball.y >= 0)
+    {
+        roles[1] = attacker(1,ball,my_robots,enemy_robots);
+        roles[2] = defender(2,ball,my_robots,enemy_robots);
+    }
+    else
+    {
+        roles[1] = defender(1,ball,my_robots,enemy_robots);
+        roles[2] = attacker(2,ball,my_robots,enemy_robots);
+    }
+
+    return roles;
+}
