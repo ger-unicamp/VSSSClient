eb30819e147c5d376859c95aa033b24ec8886bb9
Murilo-Goncalves <murilogoncalves127@gmail.com>
2020-11-09 09:00:54 -0300

    new function to compose repulsion fields

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
  */
 #include <stdio.h>
 #include <algorithm>
+#include <vector>
+#include <map>
 #include "net/robocup_ssl_client.h"
 #include "net/vss_client.h"
 #include "net/referee_client.h"
@@ -159,6 +161,40 @@ void detect_objects(fira_message::Frame detection, fira_message::Ball &ball,
     //print_info(ball, my_robots, enemy_robots, yellow);
 }
 
+pair<double, double> compose_repulsion(unsigned int moving_robot_id, vector<fira_message::Robot> my_robots, vector<fira_message::Robot> enemy_robots)
+{
+    int n_obstacles = 0;
+    double repulsion_phi;
+    double repulsion_phi_sum = 0;
+    double dist;
+    std::vector<double> distances;
+
+    for (size_t i = 0; i < my_robots.size(); ++i)
+    {
+        if (i != moving_robot_id)
+        {
+            auto obstacle =  ctrl::future_position(my_robots[i], my_robots[moving_robot_id], 0.147302);
+            ++n_obstacles;
+            repulsion_phi = (ctrl::vec2(my_robots[moving_robot_id]) - obstacle).theta();
+            repulsion_phi_sum += repulsion_phi;
+            dist = ctrl::vec2(my_robots[moving_robot_id]).distance(obstacle);
+            distances.push_back(dist);
+        }
+    }
+    for (size_t i = 0; i < enemy_robots.size(); ++i)
+    {
+        auto obstacle =  ctrl::future_position(enemy_robots[i], my_robots[moving_robot_id], 0.147302);
+        ++n_obstacles;
+        repulsion_phi = (ctrl::vec2(my_robots[moving_robot_id]) - obstacle).theta();
+        repulsion_phi_sum += repulsion_phi;
+        dist = ctrl::vec2(my_robots[moving_robot_id]).distance(obstacle);
+        distances.push_back(dist);
+    }
+
+    auto min_dist = std::min_element(distances.begin(), distances.end());
+    return make_pair(repulsion_phi_sum/n_obstacles, *min_dist);
+}
+
 int main(int argc, char *argv[])
 {
     bool yellow = false;
@@ -199,15 +235,13 @@ int main(int argc, char *argv[])
                 detect_objects(detection, ball, my_robots, enemy_robots, yellow);
 
                 // G0:0.0755485 G1:0.0691405 G2:0.443467 G3: 40 * 0.664899 + 10
-                auto robot = my_robots[0];
-                auto obstacle0 = enemy_robots[0];
-                double dist = ctrl::vec2(robot).distance(ctrl::vec2(obstacle0));
-                ctrl::vec2 spiral_vec = apf::ball_field(robot, ball, 0.0755485, 0.0691405);
-                ctrl::vec2 repulsion_vec = apf::repulsion_field(robot, obstacle0, 0.06);
-                double phi = apf::composite_field(repulsion_vec, spiral_vec, 0.0457, 0.1, dist);
+                auto robot = my_robots[0];;
+                double spiral_phi = apf::move_to_goal(robot, ball, 0.0755485, 0.0691405);
+                pair<double, double> tmp = compose_repulsion(0, my_robots, enemy_robots);
+                double phi = apf::composite_field(tmp.first, spiral_phi, 0.0413777, 0.0290801, tmp.second);
                 ctrl::vec2 apf_vec;
                 sincos(phi, &apf_vec.y, &apf_vec.x);
-                ctrl::vec2 command = ctrl::move_robot(robot, apf_vec, 0.443467, 40.0 * 0.664899 + 10.0);
+                ctrl::vec2 command = ctrl::move_robot(robot, apf_vec, 0.443467, 39.9596);
                 sim_client.sendCommand(0, command[0], command[1]);
                 // if (game_on)
                 //     sim_client.sendCommand(0, command[0], command[1]);
