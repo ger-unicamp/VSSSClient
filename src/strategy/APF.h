58f25c0645ba562f8c1e8f4dac66ce3d976e3808
rodrigocaus <digocaus@hotmail.com>
2020-11-11 00:55:25 -0300

    feature(APF): add horizontal field

--- src/strategy/APF.h
+++ src/strategy/APF.h
@@ -14,6 +14,7 @@ namespace apf
     double move_to_goal(ctrl::vec2 pos, ctrl::vec2 target, double radius, double k);
     double composite_field(double repulsion_phi, double spiral_phi, double sigma, double dmin, double R);
     double vertical_line_field(ctrl::vec2 pos, ctrl::vec2 target, double k);
+    double horizontal_line_field(ctrl::vec2 pos, ctrl::vec2 target, double k);
 }; // namespace apf
 
 #endif
