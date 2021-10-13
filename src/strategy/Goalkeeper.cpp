adb72a3a62c4a65886e37e00b141067a4163f6ff
Gabriel Silveira <gabriel@srbrl-gabrielint.(none)>
2021-10-13 10:03:00 -0300

    goalkeeper working with some bugs

--- src/strategy/Goalkeeper.cpp
+++ src/strategy/Goalkeeper.cpp
@@ -1,107 +1,11 @@
 #include "Goalkeeper.h"
+#include "Game.h"
 
 Goalkeeper::Goalkeeper(fira_message::Robot &robot)
     : Player(robot) 
 {}
 
 unsigned int Goalkeeper::lock_count = 0;
-
-/**
- * @brief Get the slope of bisector line of the triangle formed between the ball and the goal
- * 
- * @param ball_pos 
- * @return double 
- */
-double Goalkeeper::get_bisector_slope(ctrl::vec2 ball_pos)
-{
-    double y_half_size_goal = 0.2;
-    double x_goal_position = -0.75;
-
-    ctrl::vec2 top_goal_limit = ctrl::vec2( x_goal_position , y_half_size_goal);
-    ctrl::vec2 bottom_goal_limit = ctrl::vec2( x_goal_position , -y_half_size_goal);
-
-    double dist_ball_top = top_goal_limit.distance(ball_pos);
-    double dist_ball_bottom = bottom_goal_limit.distance(ball_pos);
-
-    double theta = acos( (pow(dist_ball_top , 2) + pow(dist_ball_bottom , 2) - pow(2*y_half_size_goal, 2))/(2 * dist_ball_top * dist_ball_bottom));
-    double bisector = theta/2;
-
-    double alpha, slope;
-    if (ball_pos.y > 0)
-    {
-        alpha = asin(abs(ball_pos.y - y_half_size_goal)/dist_ball_top);
-    }
-    else
-    {
-        //y position is negative
-        alpha = asin(abs(ball_pos.y + y_half_size_goal)/dist_ball_bottom);
-    }
-
-    //if vertical position of the ball is inside the goal limits
-    if (ball_pos.y < y_half_size_goal && ball_pos.y > -y_half_size_goal)
-    {   
-        slope = tan(bisector - alpha);
-    }
-    else{
-
-        slope = tan(bisector + alpha);
-        
-    }
-    //if ball's vertical position is negative, slope must be negative too
-    if (ball_pos.y < 0)
-    {
-        slope *= -1;
-    }
-
-    return slope;
-}
-
-/**
- * @brief Get the Y position of goalkeeper
- * 
- * @param ball_pos 
- * @param slope 
- * @return double 
- */
-double Goalkeeper::get_Y_position(ctrl::vec2 ball_pos, double slope)
-{
-    //intersection between X_LIMIT_GPK and bisector line
-    double linear_b = ball_pos.y - slope * ball_pos.x;
-    
-    double target_y = slope * Goalkeeper::X_LIMIT_GKP + linear_b;
-
-    return target_y;
-}
-
-
-
-/**
- * @brief defend our goal by following ball y projection using a vertical sigmoid univector field
- * 
- * @param ball_pos predicted position of ball on current frame
- * @return ctrl::vec2 
- */
-ctrl::vec2 Goalkeeper::defend_goal_from(ctrl::vec2 ball_pos)
-{
-    ctrl::vec2 target, univec;
-    double y_target, phi, slope;
-
-    //defense changes
-    slope = this->get_bisector_slope(ball_pos);
-    
-    y_target = this->get_Y_position(ball_pos, slope);
-
-    // The Goalkeeper has a Y limit
-    y_target = math::bound(y_target, -Goalkeeper::Y_LIMIT_GKP, Goalkeeper::Y_LIMIT_GKP);
-
-    // if ball is behind goalkeeper x limit, he follows ball x position
-    target = ctrl::vec2(std::min(Goalkeeper::X_LIMIT_GKP, ball_pos.x), y_target);
-    phi = this->univec_vertical_sigmoid_field(target);
-
-    sincos(phi, &univec.y, &univec.x);
-
-    return univec;
-}
 
 /**
  * @brief returns goalkeeper final motors speed
@@ -111,14 +15,15 @@ ctrl::vec2 Goalkeeper::defend_goal_from(ctrl::vec2 ball_pos)
  */
 ctrl::vec2 Goalkeeper::play(fira_message::Ball &ball)
 {
+    ctrl::vec2 target, ball_fut_pos, motors_speeds, univec;
+    double y_target, phi;
+
     // handle locked robot
     if (is_locked(this->lock_count))
     {
         bool cw = this->robot.y() < ball.y();
         return spin(cw);
     }
-    
-    ctrl::vec2 univec, motors_speeds, ball_fut_pos;
 
     ball_fut_pos = this->future_position_relative_to(ball, Goalkeeper::DT_GKP);
 
@@ -129,7 +34,27 @@ ctrl::vec2 Goalkeeper::play(fira_message::Ball &ball)
     }
     else
     {
-        univec = this->defend_goal_from(ball_fut_pos);
+        // The Goalkeeper has a Y limit
+        y_target = math::bound(ball_fut_pos.y, -Goalkeeper::Y_LIMIT_GKP, Goalkeeper::Y_LIMIT_GKP);
+
+        target = ctrl::vec2(Goalkeeper::GKP_X_AXIS, y_target);
+
+
+        //If the keeper is out of its lane, it reajusts itself
+        double tolerance = 0.1; //the keeper's lane has a margin of tolerance
+
+        if(this->robot.x() <= Goalkeeper::GKP_X_AXIS - tolerance){
+            phi = ((this->robot.y() < target.y ? 70 : 110)* PI)/180;
+        }
+        else if(this->robot.x() > Goalkeeper::GKP_X_AXIS + tolerance){
+            phi = ((this->robot.y() < target.y ? -70 : -110)* PI)/180;
+        }
+        else{
+            phi = ((this->robot.y() < target.y ? 90 : -90)* PI)/180;
+        }
+
+        sincos(phi, &univec.y, &univec.x);
+
         motors_speeds = this->move(univec);
     }
 
--- src/strategy/Goalkeeper.h
+++ src/strategy/Goalkeeper.h
@@ -6,20 +6,15 @@
 class Goalkeeper: public Player
 {
 private:
-    
-    static constexpr double X_LIMIT_GKP = -0.6675;
-    static constexpr double Y_LIMIT_GKP = 0.350;
+    static unsigned int lock_count;
+
+public:
+    static constexpr double GKP_X_AXIS = -0.7;
+    static constexpr double Y_LIMIT_GKP = 0.275;
     static constexpr double X_LIMIT_BALL = -0.52;
     static constexpr double DT_GKP = 0.245;
     static constexpr double KICK_DIST = 0.075;
 
-    static unsigned int lock_count;
-
-    ctrl::vec2 defend_goal_from(ctrl::vec2 ball_pos);
-    double get_bisector_slope(ctrl::vec2 ball_pos);
-    double get_Y_position(ctrl::vec2 ball_pos, double slope);
-
-public:
     Goalkeeper(fira_message::Robot &robot);
     ctrl::vec2 play(fira_message::Ball &ball);
 };
